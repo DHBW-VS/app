3a608a1b6668a646ff962c43c2c4b102f2e9ad8d
Robin Genz <mail@robingenz.dev>
2023-01-06 23:17:44 +0100

    test(e2e): run cypress migration

---
+++ cypress.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  videosFolder: 'cypress/videos',
+  screenshotsFolder: 'cypress/screenshots',
+  fixturesFolder: 'cypress/fixtures',
+  viewportHeight: 823,
+  viewportWidth: 411,
+  video: false,
+  env: {
+    apiBaseUrl: 'https://app-api.dhbw-vs.de/v1',
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.ts')(on, config)
+    },
+    baseUrl: 'http://localhost:4200',
+  },
+})
