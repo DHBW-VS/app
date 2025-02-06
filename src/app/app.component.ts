2e4f999116e2112ca4c1b155995c688c29962635
Robin Genz <mail@robingenz.dev>
2025-02-06 17:12:49 +0100

    chore: set status bar color to red

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Capacitor } from '@capacitor/core';
 import { SplashScreen } from '@capacitor/splash-screen';
 import { StatusBar, Style } from '@capacitor/status-bar';
+import { EdgeToEdge } from '@capawesome/capacitor-android-edge-to-edge-support';
 import { MenuController } from '@ionic/angular';
 import { CapacitorAppService, TimeoutService } from './core';
 
@@ -31,20 +32,15 @@ export class AppComponent {
   private async initializeApp(): Promise<void> {
     const isNativePlatform = Capacitor.isNativePlatform();
     if (isNativePlatform) {
-      await this.hideSplashScreen();
+      await SplashScreen.hide();
+      await EdgeToEdge.setBackgroundColor({ color: '#e2001a' });
+      // We need to add a timeout since `SplashScreen.hide()` resolves
+      // before the splash screen is actually hidden.
       await this.timeoutService.timeout(500);
-      await this.configureStatusBar();
+      const isAndroid = Capacitor.getPlatform() === 'android';
+      if (isAndroid) {
+        await StatusBar.setStyle({ style: Style.Dark });
+      }
     }
-  }
-
-  private async configureStatusBar(): Promise<void> {
-    const isAndroid = Capacitor.getPlatform() === 'android';
-    if (isAndroid) {
-      await StatusBar.setStyle({ style: Style.Light });
-    }
-  }
-
-  private hideSplashScreen(): Promise<void> {
-    return SplashScreen.hide();
   }
 }
